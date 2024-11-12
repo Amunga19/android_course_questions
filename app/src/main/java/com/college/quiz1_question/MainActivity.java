f3e50245028b5e2f62cb9cfcfb0eab0c0b09e1d1
gourteacher <ouaazs@algonquincollege.com>
2024-11-12 13:58:48 -0500

    Upgrade Gradle

--- app/src/main/java/com/college/quiz1_question/MainActivity.java
+++ app/src/main/java/com/college/quiz1_question/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 import com.college.quiz1_question.util.MyFileReader;
 import org.json.JSONArray;
 import org.json.JSONObject;

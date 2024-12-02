e73dbeba94d6d556372f6291ef9a480c2bb86a47
Amunga19 <juni0002@algonquinlive.com>
2024-12-02 14:10:35 -0500

    Works perfectly...the display with $ sign works well and the clear button clears everything

--- app/src/main/java/com/college/questions/MainActivity.java
+++ app/src/main/java/com/college/questions/MainActivity.java
@@ -3,8 +3,10 @@ package com.college.questions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         // for the number of burgers
         EditText numberStr = findViewById(R.id.numberId);
 
-        // This yextview displays the resut of multiplying
+        // This Textview displays the result of multiplying
         // the content of the numberId EditText by 5
         TextView resultTv = findViewById(R.id.resultId);
 
@@ -27,5 +29,33 @@ public class MainActivity extends AppCompatActivity {
         // Result should contain 5 multiplied by number chosen
         // Display should be prefixed with the '$' sign such as $15
         // Reset should clear all the fields
+
+
+        Button payBtn = findViewById(R.id.btnPayId);
+        Button clearBtn = findViewById(R.id.btnResetId);
+
+        payBtn.setOnClickListener(view -> {
+            // Extract the string
+            String burgerNumber = numberStr.getText().toString();
+
+            // Parse it into a double in order to do arithmetic equations
+            Double number_burg = Double.parseDouble(burgerNumber);
+
+            Double result = number_burg * 5;
+
+            //Display the results with the '$' sign
+            resultTv.setText(getString(R.string.dollar_sign, String.valueOf(result)));
+
+            Toast.makeText(this, "Payment received!", Toast.LENGTH_SHORT).show();
+        });
+
+        clearBtn.setOnClickListener(view -> {
+            //Clears the textview and the edit text
+            numberStr.setText("");
+            resultTv.setText(" ");
+        });
+
+
     }
+
 }
